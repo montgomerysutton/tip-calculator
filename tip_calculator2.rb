7343fed28bb66e9ce9290498a0ad1fd78590b0e2
Montgomery Sutton <montgomery@lupoblu.com>
2014-10-27 12:48:53 -0400

    Add Tip Calculator version 2.

---
+++ tip_calculator2.rb
@@ -0,0 +1,19 @@
+# get initial values from the user
+puts "How much did your meal cost?"
+meal = Float(gets.chomp)
+puts "What is your local tax rate in percentage?"
+tax = Float(gets.chomp)
+puts "What perecentage would you like to tip?"
+tip = Float(gets.chomp)
+
+# calculate computed values
+tax_value = meal * (tax/100)
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * (tip/100)
+total_cost = meal_with_tax+tip_value
+
+# display the results
+puts "The pre-tax cost of your meal was $" + sprintf("%.2f", meal)
+puts "At #{tax}%, tax for this meal is $" + sprintf("%.2f",tax_value)
+puts "For a #{tip}% tip, you should leave $" + sprintf("%.2f",tip_value)
+puts "The grand total for this meal is then $" + sprintf("%.2f",total_cost)
